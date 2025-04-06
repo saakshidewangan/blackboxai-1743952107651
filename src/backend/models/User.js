70c3c821ac8b21425b1895b6ae6e9f94b28e8d10
Saakshi Dewangan <126800946+saakshidewangan@users.noreply.github.com>
2025-04-06 20:39:01 +0530

    Add src/backend/models/User.js 1743952141014

---
+++ src/backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firebaseUid: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
